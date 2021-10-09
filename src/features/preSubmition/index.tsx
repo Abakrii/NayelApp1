10e92f579519d641fd0228c6c7c7e758d7355a25
AbdelrahmanB@mashreq.com <AbdelrahmanB@mashreq.com>
2021-10-09 13:06:23 +0400

    finish the form ui

--- src/features/preSubmition/index.tsx
+++ src/features/preSubmition/index.tsx
@@ -1,22 +1,71 @@
-import React from 'react';
-import {View, TextInput} from 'react-native';
-
+import React, {useState} from 'react';
+import {View, TextInput, Button, Text} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import {Picker} from '@react-native-picker/picker';
+import styles from './styels';
+import {Content} from '../../core/components';
 const PreSubmition = () => {
-  const [value, onChangeText] = React.useState('Useless Multiline asdasdsad');
+  const {
+    mainView,
+    textView,
+    checkboxContainer,
+    checkbox,
+    checkboxLabel,
+    pickerView,
+    descriptionStyle,
+    buttonView,
+  } = styles || {};
+  const [email, onChangeMail] = useState('');
+  const [isJoke, setIsJokeCheckBox] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('');
+  const [description, setDescription] = useState('');
   return (
-    <View
-      style={{
-        backgroundColor: value,
-        borderBottomColor: '#000000',
-        borderBottomWidth: 1,
-      }}>
-      <TextInput
-        multiline
-        numberOfLines={4}
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        style={{padding: 10}}
-      />
+    <View style={mainView}>
+      <Content>
+        <View style={textView}>
+          <TextInput
+            multiline
+            onChangeText={text => onChangeMail(text)}
+            value={email}
+            placeholder={'please enter your email address'}
+          />
+        </View>
+        <View style={checkboxContainer}>
+          <CheckBox
+            disabled={false}
+            value={isJoke}
+            onValueChange={newValue => setIsJokeCheckBox(newValue)}
+            style={checkbox}
+          />
+          <Text style={checkboxLabel}>Is Joke?</Text>
+        </View>
+
+        <Picker
+          selectedValue={selectedValue}
+          style={pickerView}
+          onValueChange={itemValue => setSelectedValue(itemValue)}>
+          <Picker.Item label="Teacher" value="Teacher" />
+          <Picker.Item label="Blogger" value="Blogger" />
+        </Picker>
+        <View style={textView}>
+          <TextInput
+            multiline
+            numberOfLines={4}
+            onChangeText={text => setDescription(text)}
+            value={description}
+            style={descriptionStyle}
+            placeholder={'please enter your description'}
+          />
+        </View>
+        <View style={buttonView}>
+          <Button
+            disabled={false}
+            onPress={() => console.log('test the button')}
+            title={'Submit the form'}
+            color="coral"
+          />
+        </View>
+      </Content>
     </View>
   );
 };
